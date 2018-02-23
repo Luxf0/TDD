cecbe6d8c5d8316f3b414ba3cb987bf08d5293ba
Micah--company@17 <lu13723751426@gmail.com>
2018-02-24 00:35:10 +0800

    the test of FT was first commit

--- functional_test.py
+++ functional_test.py
@@ -1,5 +1,19 @@
 from selenium import webdriver
+import unittest
 
-driver=webdriver.Chrome("F:\TDD")
-driver.get
+class FirstVisitor(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Chrome("F:\TDD\chromedriver.exe")
+        self.browser.implicitly_wait(3)
 
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_First(self):
+        self.browser.get("http://localhost:8000")
+        self.assertIn("To-Do",self.browser.title,msg='browser title was :'+ self.browser.title)
+        #self.fail("finish the test!")
+
+
+if __name__ == "__main__":
+    unittest.main()
